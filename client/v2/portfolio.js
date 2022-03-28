552a28663eb1f12831b050be2ae8db1fcf884c33
tsolus <tristan.solus@gmail.com>
2022-03-28 10:28:14 +0200

    feat(display): workshop2

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -4,13 +4,43 @@
 // current products on the page
 let currentProducts = [];
 let currentPagination = {};
+let filterBrand = "noFilter";
+let filterRecent = "no";
+let filterReasonable = "no";
+let sortFilter = "notSorted"; 
+let setFavorite = new Set();
+let filterFavorite = "no";
 
 // instantiate the selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
+const spanNbNewProducts = document.querySelector('#nbNewProducts')
+const spanP50 = document.querySelector('#p50');
+const spanP90 = document.querySelector('#p90');
+const spanP95 = document.querySelector('#p95');
+const spanLastRelease = document.querySelector('#last_release');
+const selectFilterRecent = document.querySelector('#recent-select');
+const selectFilterReasonable = document.querySelector('#reasonable-select');
+const selectFilterFavorite = document.querySelector("#favorite-select");
+const selectSort = document.querySelector('#sort-select');
 
+
+
+const date = new Date();
+function isNew(product){
+	const release = new Date(product.released);
+	var diff = Math.abs((date - release)/(7*24*60*60*1000));
+	//console.log(diff);
+	if (diff < 2) {
+		return "True";
+	}
+	else
+	{
+		return "False";
+	};
+}
 /**
  * Set global value
  * @param {Array} result - products to display
@@ -30,22 +60,73 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      `https://clear-fashion-two-zeta.vercel.app/Products?page=${page}&size=${size}`
     );
     const body = await response.json();
-
+    var groupbyBrand = body.data.result.reduce(function(groups, item) {
+      const val = item["brand"]
+      groups[val] = groups[val] || []
+      groups[val].push(item)
+      return groups
+    }, {});
+    var selectBox = document.getElementById('brand-select');
+    selectBox.options.length=0;
+    selectBox.options.add( new Option("-", "noFilter", false));
+    for (var i =0, l = Object.keys(groupbyBrand).length; i< l; i++){
+      var option = Object.keys(groupbyBrand)[i];
+      selectBox.options.add( new Option(option, option, false));
+    }
+    selectBox.options.add(new Option("No filter", "noFilter", false));
+    if (filterBrand!="noFilter"){
+      body.data.result = groupbyBrand[filterBrand]
+    }
+    if (filterReasonable == "yes"){
+      body.data.result = body.data.result.filter(a => a.price<50)
+    }
+    if (filterRecent == "yes"){
+      body.data.result = body.data.result.filter(a => isNew(a) == "True");
+    }
+    if (filterFavorite == "yes"){
+      body.data.result = body.data.result.filter(a => setFavorite.has(a._id) == true);
+    }
     if (body.success !== true) {
       console.error(body);
       return {currentProducts, currentPagination};
     }
-
+    switch(sortFilter){
+      case 'price-asc':
+        body.data.result = body.data.result.sort((a,b)=> a.price - b.price);
+        break;
+      case 'price-desc':
+        body.data.result = body.data.result.sort((a,b)=> b.price - a.price);
+        break;
+      case 'date-asc':
+        body.data.result = body.data.result.sort(function(a,b){
+          if (a.released<b.released) {
+            return -1;
+          } else {
+            return 1;
+        };});
+        break;
+      case 'date-desc':
+        body.data.result = body.data.result.sort(function(a,b){
+          if (a.released>b.released) {
+            return -1;
+          } else {
+            return 1;
+        };});
+        break;    
+    }
     return body.data;
   } catch (error) {
     console.error(error);
     return {currentProducts, currentPagination};
   }
+  
 };
 
+const selectBrand = document.querySelector('#brand-select');
+//console.log(selectBrand);
 /**
  * Render list of products
  * @param  {Array} products
@@ -56,10 +137,11 @@ const renderProducts = products => {
   const template = products
     .map(product => {
       return `
-      <div class="product" id=${product.uuid}>
+      <div class="product" id=${product._id}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
+        <a target="_blank" href="${product.link}">${product.name}</a>
         <span>${product.price}</span>
+        <input type="checkbox" id=${product._id} value ="Favorite">
       </div>
     `;
     })
@@ -90,18 +172,61 @@ const renderPagination = pagination => {
  * Render page selector
  * @param  {Object} pagination
  */
-const renderIndicators = pagination => {
-  const {count} = pagination;
+function percentile(nb,products){
+    let pos = Math.round(products.length * (1-(nb/100)));
+    let cop = JSON.parse(JSON.stringify(products)).sort((a, b) => a.price-b.price);
+    if (pos-1 < 0){ pos = 1;};
+    return cop[pos-1].price;
+    
+};
 
-  spanNbProducts.innerHTML = count;
+function sortDate(products){
+  var sort = JSON.parse(JSON.stringify(products)).sort(function(a,b){
+    if (a.released<b.released) {
+      return -1;
+    } else {
+      return 1;
+  };});
+  return sort;
+}
+
+const renderIndicators = (pagination, products) => {
+  spanNbNewProducts.innerHTML = products.filter(a => isNew(a) == "True").length;
+  spanNbProducts.innerHTML = products.length;
+  spanP50.innerHTML = percentile(50, products);
+  spanP90.innerHTML = percentile(90, products);
+  spanP95.innerHTML = percentile(95, products);
+  let t = sortDate(products);
+  spanLastRelease.innerHTML = t[t.length-1].released;
 };
 
 const render = (products, pagination) => {
   renderProducts(products);
   renderPagination(pagination);
-  renderIndicators(pagination);
+  renderIndicators(pagination, products);
+  const checkboxes = document.querySelectorAll("input[value=Favorite]");
+  checkboxes.forEach(function(checkbox){
+    checkbox.addEventListener('change' ,function() {
+      let x = Array.from(checkboxes)
+      .filter(i => i.checked)
+      .map(i => i.id);
+
+      x.forEach(function(t){
+        setFavorite.add(t);
+        });
+      if (checkbox.checked == false)
+      {
+        setFavorite.delete(checkbox.id);
+      };
+    })
+  });
 };
 
+
+
+
+
+//console.log(checkboxes);
 /**
  * Declaration of all Listeners
  */
@@ -109,16 +234,58 @@ const render = (products, pagination) => {
 /**
  * Select the number of products to display
  */
-selectShow.addEventListener('change', async (event) => {
-  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value));
-
-  setCurrentProducts(products);
-  render(currentProducts, currentPagination);
+selectShow.addEventListener('change', event => {
+  currentPagination.pageSize = parseInt(event.target.value);
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination));
 });
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const products = await fetchProducts();
-
-  setCurrentProducts(products);
-  render(currentProducts, currentPagination);
+selectPage.addEventListener('change', event => {
+  currentPagination.currentPage = parseInt(event.target.value);
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+  .then(setCurrentProducts)
+  .then(() => render(currentProducts, currentPagination));
 });
+
+selectBrand.addEventListener('change', event => {
+  filterBrand = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+  .then(setCurrentProducts)
+  .then(() => render(currentProducts, currentPagination));
+});
+
+selectFilterReasonable.addEventListener('change', event => {
+  filterReasonable = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+  .then(setCurrentProducts)
+  .then(() => render(currentProducts, currentPagination));
+});
+
+selectFilterRecent.addEventListener('change', event => {
+  filterRecent = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+  .then(setCurrentProducts)
+  .then(() => render(currentProducts, currentPagination));
+});
+
+selectSort.addEventListener('change', event => {
+  sortFilter = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+  .then(setCurrentProducts)
+  .then(() => render(currentProducts, currentPagination));
+});
+
+selectFilterFavorite.addEventListener('change', event => {
+  filterFavorite = event.target.value;
+  fetchProducts(currentPagination.currentPage, currentPagination.pageSize)
+  .then(setCurrentProducts)
+  .then(() => render(currentProducts, currentPagination));
+});
+
+
+document.addEventListener('DOMContentLoaded', () =>
+  fetchProducts()
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts, currentPagination))
+);
