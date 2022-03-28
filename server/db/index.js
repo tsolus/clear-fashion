79c56c7aaaef5d82ae03b441bf2bda5aebaf1609
tsolus <tristan.solus@gmail.com>
2022-03-28 14:50:55 +0200

    feat(scrape): workshop2

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const MONGODB_DB_NAME = 'clearfashion';
 const MONGODB_COLLECTION = 'products';
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb+srv://<tsolus>:<Benjie06>@clear-fashion.sorfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 let client = null;
 let database = null;
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,14 +1,67 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
+const dedicatedbrand = require('./sites/dedicatedbrand');
+const adresseParis = require('./sites/adresseparis');
+const montlimart = require('./sites/montlimart');
+const fetch = require("node-fetch");
+const fs = require('fs');
+const {'v5': uuidv5} = require('uuid');
+const { json } = require('express');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+//https://adresse.paris/630-toute-la-collection
+//https://www.dedicatedbrand.com/en/loadfilter?category=men%2Fnews
+//https://www.montlimart.com/toute-la-collection.html
+
+async function sandbox (eshop) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
+    var products = [];
+    var ad = "";
+    switch (eshop){
+      case "montlimart":
+        var i = 1;
+        var varr= "";
+        while(true){
+          let prod = await montlimart.scrape('https://www.montlimart.com/toute-la-collection.html' + '?p=' + i);
+          if (prod[0]["_id"]== varr){
+            ad = JSON.stringify(products);
+            fs.writeFile('sites/montlimart.json', ad, (err)=> {if(err){throw err;} console.log("JSON saved : Montlimart");})   
+            break;
+          } 
+          Array.prototype.push.apply(products, prod);
+          varr = prod[0]["_id"];
+          i = i+1;
+        }
+        break;
+      case 'dedicated':
+        const result = await fetch('https://www.dedicatedbrand.com/en/loadfilter?category=men%2Fall-men');
+        const x   = await result.json();
+        prod = x.products; 
+        products = [];
+        prod.splice(0,3);
+        prod.forEach(function(a){
+          if (a.length == undefined){
+            products.push({'link' : 'https://www.dedicatedbrand.com/en/' + a.canonicalUri, "brand" : "dedicated", 
+            "price" : a.price.priceAsNumber, "name" : a.name, "photo" : a.image[0], '_id': uuidv5(a.canonicalUri, uuidv5.URL)});
 
-    const products = await dedicatedbrand.scrape(eshop);
+          }
+        });       
+        ad = JSON.stringify(products);
+        //console.log(products);
+        fs.writeFile('sites/dedicated.json', ad, (err)=> {if(err){throw err;} console.log("JSON saved : dedicated");})     
+        break;
+      case 'adresse':
+        const product1 = await adresseParis.scrape("https://adresse.paris/630-toute-la-collection");
+        //console.log(product1);
+        const product2 = await adresseParis.scrape("https://adresse.paris/630-toute-la-collection?p=2")
+        products = product1.concat(product2);
+        ad = JSON.stringify(products);
+        fs.writeFile('sites/adresseParis.json', ad, (err)=> {if(err){throw err;} console.log("JSON saved : Adresse");})
+        break;
+    }
 
-    console.log(products);
+    //console.log(products);
     console.log('done');
+    return products;
     process.exit(0);
   } catch (e) {
     console.error(e);
@@ -17,5 +70,5 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 }
 
 const [,, eshop] = process.argv;
-
-sandbox(eshop);
+const web = ["montlimart", "dedicated", "adresse"];
+web.forEach(a => sandbox(a));
